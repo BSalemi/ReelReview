6bc7ac8fab02134c5196aee97c9a4273721adf03
BSalemi <briandsalemi@gmail.com>
2025-03-01 09:09:48 -0500

    setting up boiler plate to dynamically render the stars in StarRating component and setting up style of container

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,3 +1,17 @@
 export default function StarRating() {
-  return <h1>Hello</h1>;
+  const containerStyle = {
+    display: "flex",
+    alignItems: "center",
+    gap: "16px",
+  };
+  return (
+    <div style={containerStyle}>
+      <div>
+        {Array.from({ length: 5 }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+      </div>
+      <p>10</p>
+    </div>
+  );
 }
