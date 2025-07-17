28e2b519bb5594ff6559578c25a5dc36a4df314b
BSalemi <briandsalemi@gmail.com>
2025-07-17 14:38:46 -0400

    added react-router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
@@ -8,11 +9,13 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    {/* <StarRating
+    <BrowserRouter>
+      <App />
+      {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
       defaultRating={3}
     /> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
